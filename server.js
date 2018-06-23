d023caa670feed49fc22e1275cc3531b0099cb60
Vibhor Bakshi <vibhor.bakshi1998@gmail.com>
2018-06-23 12:24:34 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,49 @@
+const express=require('express');
+const hbs=require('hbs');
+const fs=require('fs');
+var app=express();
+hbs.registerPartials(__dirname+'/views/partials');
+app.set('view engine','hbs');
+app.use((req,res,next)=>{
+	var now= new Date().toString();
+	var log=`${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFileSync('server.log',log+'\n',(error)=>{
+		if(error){
+			console.log("Unable to log");
+		}
+	})
+	next();
+});
+// app.use((req,res,next)=>{
+// 	res.render(('maintain.hbs'))
+// });
+app.use(express.static(__dirname+'/public'));
+
+hbs.registerHelper('currentyear',()=>{
+	return new Date().getFullYear();
+})
+app.get('/',(req,res)=>{
+// res.send('Hello Express');
+res.render('home.hbs',{
+	pageTitle:"Home",
+	header:'Home',
+	message:"Welcome Message. "
+})
+});
+app.get('/abouts',(req,res)=>{
+	res.render('abouts.hbs',{
+		pageTitle:'About Page',
+		header:'About',
+	});
+});
+
+app.get('/bad',(req,res)=>{
+	res.send({
+		error:'Unable to connect'
+	});
+});
+
+app.listen(3000,()=>{
+	console.log('Server is up on port 3000');
+});
